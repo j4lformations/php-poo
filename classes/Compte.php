30a05ec1009e5909423e00f72f1103917c776b68
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2021-11-30 16:49:37 +0100

    Methode d'instance

--- classes/Compte.php
+++ classes/Compte.php
@@ -6,6 +6,57 @@
     class Compte
     {
         //Les attributs de la classes
-        public string $numero;
-        public float $solde;
+        // On parle d'encapsulation des attributs
+        private string $numero;
+        private float $solde;
+
+        // LE CONSTRUCTEUR
+        public function __construct(string $numero, float $solde)
+        {
+            $this->numero = $numero;
+            $this->solde = $solde;
+        }
+
+        //SETTERS OU MODIFICATEURS OU ACCES EN ECRITURE
+//        public function setNumero(string $numero): void
+//        {
+//            $this->numero = $numero;
+//        }
+//
+//        public function setSolde(float $solde): void
+//        {
+//            $this->solde = $solde;
+//        }
+
+        //GETTERS OU ACCES EN LECTURE
+        public function getNumero(): string
+        {
+            return $this->numero;
+        }
+
+        public function getSolde(): float|int
+        {
+            return $this->solde;
+        }
+
+        public function deposer(float $montant): void
+        {
+            if ($montant > 0) {
+                // $this->solde = $this->solde + $montant;
+                $this->solde += $montant;
+            }
+        }
+
+        public function retirer(float $montant): void
+        {
+            if ($montant > 0) {
+                $this->solde -= $montant;
+            }
+        }
+
+        // Permet d'afficher les sous forme de chaine de caracteres
+        public function __toString(): string
+        {
+            return "Compte[" . $this->numero . ", " . $this->solde . "€]";
+        }
     }
--- index.php
+++ index.php
@@ -10,51 +10,81 @@
     require_once 'classes/Compte.php';
     require_once 'classes/Personne.php';
 
-    // Je cree une instance de Compte avec le mot clé new, ou je crée un nouvel objet de type Compte
-    $cptKim = new Compte();
+    /*
+       // Je cree une instance de Compte avec le mot clé new, ou je crée un nouvel objet de type Compte
+       $cptKim = new Compte();
 
-    //Je defini les valeurs des attributs de l'objet compte
-    $cptKim->numero = "N°001";
-    $cptKim->solde = 1000.5;
+       //Je defini les valeurs des attributs de l'objet compte
+       $cptKim->numero = "N°001";
+       $cptKim->solde = 1000.5;
+       var_dump($cptKim);
+
+       echo '<br/>';
+
+       $cptIlan = new Compte();
+       $cptIlan->numero = "N°002";
+       $cptIlan->solde = 2000;
+       var_dump($cptIlan);
+
+       echo '<br/>';
+
+       $cptBilal = new Compte();
+       $cptBilal->numero = "N°003";
+       $cptBilal->solde = 6005;
+       var_dump($cptBilal);
+
+       echo '<br/>';
+
+       $cptNikolic = $cptIlan;
+       var_dump($cptNikolic);
+
+       echo '<br/>';
+
+       $cptNikolic->solde = 10000;
+       var_dump($cptIlan);
+
+       $a = 25;
+       $b = $a;
+
+       echo '<br/>';
+       var_dump($a);
+
+       echo '<br/>';
+       var_dump($b);
+
+       $a = "joachim";
+
+       echo '<br/>';
+       var_dump($a);
+
+       echo '<br/>';
+       var_dump($b);
+    */
+
+//=========================================================
+
+//    $cptKim = new Compte();
+//    //$cptKim->numero = "N°001";      // public --> Les valeurs des attributs sont publiques
+//    //$cptKim->solde = 1500;          // public --> Les valeurs des attributs sont publiques
+//
+//    $cptKim->setNumero("N°001");
+//
+//    var_dump($cptKim);
+//    //$cptKim->numero = "N°002";
+//    //var_dump($cptKim);
+
+    $cptKim = new Compte("001", 5000);
     var_dump($cptKim);
 
-    echo '<br/>';
+    echo "<br/>";
+    echo "<h2>Numero = {$cptKim->getNumero()}</h2>";
+    echo "<h2>Solde = {$cptKim->getSolde()} €</h2>";
 
-    $cptIlan = new Compte();
-    $cptIlan->numero = "N°002";
-    $cptIlan->solde = 2000;
-    var_dump($cptIlan);
+    echo "<h2>" . $cptKim . "</h2>";
 
-    echo '<br/>';
+    $cptKim->deposer(1550);
+    echo "<h2>" . $cptKim . "</h2>";
 
-    $cptBilal = new Compte();
-    $cptBilal->numero = "N°003";
-    $cptBilal->solde = 6005;
-    var_dump($cptBilal);
-
-    echo '<br/>';
-
-    $cptNikolic = $cptIlan;
-    var_dump($cptNikolic);
-
-    echo '<br/>';
-
-    $cptNikolic->solde = 10000;
-    var_dump($cptIlan);
-
-    $a = 25;
-    $b = $a;
-
-    echo '<br/>';
-    var_dump($a);
-
-    echo '<br/>';
-    var_dump($b);
-
-    $a = "joachim";
-
-    echo '<br/>';
-    var_dump($a);
-
-    echo '<br/>';
-    var_dump($b);
+//    $cptKim->retirer(-500);
+    $cptKim->retirer(2000);
+    echo "<h2>" . $cptKim . "</h2>";
